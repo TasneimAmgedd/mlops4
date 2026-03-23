a27d973ac6490ea7e5cfe4c104c6e8ebec60b2fa
tasneimamged <tasneimshehab6@gmail.com>
2026-03-23 18:07:03 +0200

    Update check_threshold.py

--- check_threshold.py
+++ check_threshold.py
@@ -1,5 +1,5 @@
 import sys
-accuracy = 0.90 # لو عايزاه ينجح خليها 0.90، لو عايزاه يفشل خليها 0.80
+accuracy = 0.80
 if accuracy < 0.85:
     print("Failed: Accuracy below threshold")
     sys.exit(1)
